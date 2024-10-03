c68d2c5158a6783c8ac2bed853bb234ba27e4e66
arkorrr <120219441+arkorrr@users.noreply.github.com>
2024-10-04 00:43:35 +0300

    Update Task4.cs

--- delegate(methode extension 2.0)/Task4.cs
+++ delegate(methode extension 2.0)/Task4.cs
@@ -1,13 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace delegate_methode_extension_2._0_
-{
-    internal class Task4
-    {
-
-    }
-}
